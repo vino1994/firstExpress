23739f5ee3be5ff07904f6367700f7ea7a077f5c
怪兽产品 <zhangdeyang@enmonster.com>
2018-08-03 16:35:25 +0800

    change

--- app.js
+++ app.js
@@ -50,7 +50,11 @@ app.all('*', function (req, res, next) {
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Content-Type", "application/json;charset=utf-8");
-    next();
+    if (req.method === 'OPTIONS') {
+        res.send(200)
+    } else {
+        next()
+    }
 });
 
 
