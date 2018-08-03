8b8f366a11db6854390d488eeceb609fdae57695
怪兽产品 <zhangdeyang@enmonster.com>
2018-08-03 17:10:34 +0800

    跨域

--- app.js
+++ app.js
@@ -8,6 +8,22 @@ const config = require('config-lite')(__dirname)
 
 var app = express();
 
+
+//设置跨域访问
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length,Authorization,Accept,X-Requested-With,yourHeaderFeild');
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Content-Type", "application/json;charset=utf-8");
+    if (req.method === 'OPTIONS') {
+        res.send(200)
+    } else {
+        next()
+    }
+});
+
+
 //使用上面引入的包
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -41,23 +57,6 @@ app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
-
-
-//设置跨域访问
-app.all('*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length,Authorization,Accept,X-Requested-With,yourHeaderFeild');
-    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Content-Type", "application/json;charset=utf-8");
-    if (req.method === 'OPTIONS') {
-        res.send(200)
-    } else {
-        next()
-    }
-});
-
-
 
 //配置服务端口
 
