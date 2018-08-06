cbeac97433e1ecde54dcd70ac87d28601c24d3d8
怪兽产品 <zhangdeyang@enmonster.com>
2018-08-06 18:08:17 +0800

    xiugai

--- config/default.js
+++ config/default.js
@@ -1,8 +1,8 @@
 module.exports = {
     port: 3000,
     session: {
-        secret: 'vino',
-        key: 'vino',
+        secret: 'myblog',
+        key: 'myblog',
         maxAge: 2592000000
     },
     mongodb: ''
