8e184b71c05fd4e8136f861c33779a2530b1b087
怪兽产品 <zhangdeyang@enmonster.com>
2018-08-06 17:11:40 +0800

    x

--- routes/wx.js
+++ routes/wx.js
@@ -2,7 +2,7 @@ const express = require('express');
 const weixin = require('weixin-api');
 const router = express.Router();
 
-//weixin
+//微信config
 router.get('/config', function (req, res) {
     // 签名成功
     if (weixin.checkSignature(req)) {
