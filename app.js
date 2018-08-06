407dd4a40d7cddb448cdc0465ce056ad61854ef6
怪兽产品 <zhangdeyang@enmonster.com>
2018-08-06 17:13:53 +0800

    x

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Content-Type", "application/json;charset=utf-8");
     if (req.method === 'OPTIONS') {
-        res.send(200)
+        res.send(200);
     } else {
-        next()
+        next();
     }
 });
 
